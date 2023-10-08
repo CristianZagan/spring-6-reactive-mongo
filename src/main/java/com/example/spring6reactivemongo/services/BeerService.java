7feedc5ba6cfeb72ff714e41b9533a3e8f377f2b
CristianZagan <cristizagan85@gmail.com>
2023-10-09 00:35:18 +0300

    added repositories.BeerRepository CustomerRepository added services.BeerService added services.BeerServiceImpl added config.MongoConfig

---
+++ src/main/java/com/example/spring6reactivemongo/services/BeerService.java
@@ -0,0 +1,13 @@
+package com.example.spring6reactivemongo.services;
+
+
+import com.example.spring6reactivemongo.model.BeerDTO;
+import reactor.core.publisher.Mono;
+
+
+public interface BeerService {
+
+    Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDTO);
+
+    Mono<BeerDTO> getById(String beerId);
+}
